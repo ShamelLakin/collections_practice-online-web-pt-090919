122ff2c32d068411b613fe3535652bafb25cc3b1
Shamel Lakin <christlife4u@gmail.com>
2019-09-20 17:32:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(num)
+  num.sort  do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(num)
+  num.sort do |x, y| 
+  -(x <=> y)
+  end
+end
+
+def sort_array_char_count(num)
+   num.sort do |x, y| 
+     x.length <=> y.length
+  end
+end 
+
+def swap_elements(num)
+  num.sort  do |a[2], b[3]|
+    a <=> b
+  end
+end
